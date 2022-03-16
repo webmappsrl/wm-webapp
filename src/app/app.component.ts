8e217828a013cf8b6ecab279026d7469be381f95
bongiu <peppedeka@gmail.com>
2022-03-16 16:05:47 +0100

    card webapp.0.0.08.11
    give the THEME by state managment.
    renaming css variables from --ion to --wm
    create css variables in bootstrap section and overrides default

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,35 @@
-import {Component} from '@angular/core';
+import {DOCUMENT} from '@angular/common';
+import {Component, Inject, ViewEncapsulation} from '@angular/core';
 import {Store} from '@ngrx/store';
 import {Observable} from 'rxjs';
+import {take} from 'rxjs/operators';
 import {LanguagesService} from './services/languages.service';
 import {loadConf} from './store/conf.actions';
 import {IConfRootState} from './store/conf.reducer';
-import {confAPP, confTHEME} from './store/conf.selector';
-
+import {confTHEMEVariables} from './store/conf.selector';
 @Component({
   selector: 'webmapp-app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
-  APP$: Observable<any> = this._store.select(confAPP);
-  THEME$: Observable<any> = this._store.select(confTHEME);
-  constructor(private _languagesService: LanguagesService, private _store: Store<IConfRootState>) {
+  confTHEMEVariables$: Observable<any> = this._store.select(confTHEMEVariables);
+
+  constructor(
+    @Inject(DOCUMENT) private _document: Document,
+    private _languagesService: LanguagesService,
+    private _store: Store<IConfRootState>,
+  ) {
     this._languagesService.initialize();
     this._store.dispatch(loadConf());
+    this.confTHEMEVariables$.pipe(take(2)).subscribe(css => this._setGlobalCSS(css));
+  }
 
-    this.APP$.subscribe(asd => {
-      console.log(asd);
-    });
-    this.THEME$.subscribe(asd => {
-      console.log(asd);
+  private _setGlobalCSS(css: {[name: string]: string | number}) {
+    const rootDocument = this._document.querySelector(':root');
+    Object.keys(css).forEach(element => {
+      (rootDocument as any).style.setProperty(element, `${css[element]}`);
     });
   }
 }
--- src/app/store/conf.selector.ts
+++ src/app/store/conf.selector.ts
@@ -1,8 +1,12 @@
 import {createFeatureSelector, createSelector} from '@ngrx/store';
+import {BehaviorSubject} from 'rxjs';
+import {getCSSVariables} from '../functions/theme';
 import {confFeatureKey, IConfRootState} from './conf.reducer';
 import {IConfState} from './model';
 
-const confFeature = createFeatureSelector<IConfRootState, IConfState>(confFeatureKey);
-
+const confFeature = createFeatureSelector<IConfState>(confFeatureKey);
 export const confAPP = createSelector(confFeature, state => state.APP);
 export const confTHEME = createSelector(confFeature, state => state.THEME);
+export const confTHEMEVariables = createSelector(confTHEME, (theme: ITHEME) =>
+  getCSSVariables(theme),
+);

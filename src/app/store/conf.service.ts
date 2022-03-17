ac456c0203a10befc4b72c24c2d8cd401a8c0194
bongiu <peppedeka@gmail.com>
2022-03-17 10:21:17 +0100

    fix(config): get config geohub id by url

--- src/app/store/conf.service.ts
+++ src/app/store/conf.service.ts
@@ -1,17 +1,41 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {environment} from '../../environments/environment';
+import {GEOHUB_DOMAIN, GEOHUB_PROTOCOL} from '../constants/geohub';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ConfService {
-  private _apiConf = environment.confRoot;
+  private _geohubAppId: number = 4;
 
-  constructor(private _http: HttpClient) {}
+  constructor(private _http: HttpClient) {
+    const hostname: string = window.location.hostname;
+    if (hostname.indexOf('localhost') < 0) {
+      const newGeohubId = parseInt(hostname.split('.')[0], 10);
+      if (!Number.isNaN(newGeohubId)) {
+        this._geohubAppId = newGeohubId;
+      }
+    }
+  }
 
-  getConf(): Observable<ICONF> {
-    return this._http.get<ICONF>(`${this._apiConf}/config.json`);
+  public get configUrl(): string {
+    return `${this._geohubApiBaseUrl}config`;
+  }
+
+  public get geohubAppId(): number {
+    return this._geohubAppId;
+  }
+
+  public get vectorStyleUrl(): string {
+    return `${this._geohubApiBaseUrl}vector_style`;
+  }
+
+  private get _geohubApiBaseUrl(): string {
+    return `${GEOHUB_PROTOCOL}://${GEOHUB_DOMAIN}/api/app/webmapp/${this._geohubAppId}/`;
+  }
+
+  public getConf(): Observable<ICONF> {
+    return this._http.get<ICONF>(`${this._geohubApiBaseUrl}config.json`);
   }
 }

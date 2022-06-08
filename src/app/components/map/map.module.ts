ddb801dfdc2f7c8994b945e825711d7a335bce6c
bongiu <peppedeka@gmail.com>
2022-06-08 12:12:38 +0200

    interventi_su_mappa_web.222.01.2

--- src/app/components/map/map.module.ts
+++ src/app/components/map/map.module.ts
@@ -3,10 +3,11 @@ import {CommonModule} from '@angular/common';
 import {MapComponent} from './map.component';
 import {IonicModule} from '@ionic/angular';
 import {WMCommonModule} from '../common/wm-common.module';
+import {NavMapTrackDirective} from './track.directive';
 // import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
-  declarations: [MapComponent],
+  declarations: [MapComponent, NavMapTrackDirective],
   imports: [
     CommonModule,
     WMCommonModule,

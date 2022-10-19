a53171c1bc360770a1bc783fcf1331d97573dedd
bongiu <peppedeka@gmail.com>
2022-10-19 13:47:28 +0200

    add mapcore sub modules

--- src/app/shared/map-core/zIndex.ts
+++
@@ -1,5 +0,0 @@
-export const TRACK_ZINDEX = 450;
-export const SELECTED_TRACK_ZINDEX = 460;
-export const FLAG_TRACK_ZINDEX = 470;
-export const POINTER_TRACK_ZINDEX = 480;
-export const POSITION_ZINDEX = 500;

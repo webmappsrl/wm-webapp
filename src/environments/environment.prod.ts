6bd1318ad5a99d8697fcb4320c8694bcf229c8c8
bongiu <peppedeka@gmail.com>
2023-01-18 12:39:27 +0100

    ...

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   geohubId: 15,
   api: 'https://geohub.webmapp.it',
-  graphhopperHost: 'https://graphhopper.sviluppo.lunet.it/',
+  // graphhopperHost: 'https://graphhopper.sviluppo.lunet.it/',
+  graphhopperHost: 'https://graphhopper.webmapp.it/',
 };

4450761241cb67ed7f058e75af99ff3b47d68990
bongiu <peppedeka@gmail.com>
2023-01-31 13:38:09 +0100

    change old  trans pipe with wmcore pipe

--- src/app/pipes/pipe.module.ts
+++ src/app/pipes/pipe.module.ts
@@ -1,13 +1,13 @@
+import {WmPipeModule} from './../shared/wm-core/pipes/pipe.module';
 import {BuildSvgDirective} from './build-svg.directive';
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
-import {WebmappTranslatePipe} from './webmapp-translate.pipe';
 import {WmGetFilterIcnPipe} from './webmapp-get-filter-icn.pipe';
 import {WmGetIcnPipe} from './webmapp-get-icn.pipe';
 
 @NgModule({
-  declarations: [WebmappTranslatePipe, WmGetIcnPipe, WmGetFilterIcnPipe, BuildSvgDirective],
-  imports: [CommonModule],
-  exports: [WebmappTranslatePipe, WmGetIcnPipe, WmGetFilterIcnPipe, BuildSvgDirective],
+  declarations: [WmGetIcnPipe, WmGetFilterIcnPipe, BuildSvgDirective],
+  imports: [CommonModule, WmPipeModule],
+  exports: [WmGetIcnPipe, WmGetFilterIcnPipe, BuildSvgDirective, WmPipeModule],
 })
 export class PipeModule {}

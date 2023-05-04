1ae2ed65af42ae4f51aabfdcfdad81c10e63b59e
bongiu <peppedeka@gmail.com>
2023-05-04 11:17:40 +0200

    ...

--- src/app/types/track-elevation-chart.d.ts
+++ src/app/types/track-elevation-chart.d.ts
@@ -1,11 +1,11 @@
-import { CGeojsonLineStringFeature } from '../classes/features/cgeojson-line-string-feature';
-import { ILocation } from './location';
+import {CGeojsonLineStringFeature} from '../classes/features/cgeojson-line-string-feature';
+import {ILocation} from './location';
 
 export interface ITrackElevationChartStyle {
   backgroundColor: string;
 }
 
 export interface ITrackElevationChartHoverElements {
-  location: ILocation;
+  location: any;
   track?: CGeojsonLineStringFeature;
 }

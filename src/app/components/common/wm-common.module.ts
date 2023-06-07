7919047b715bef0f479be7300f2171dc4563fde3
bongiu <peppedeka@gmail.com>
2023-06-07 09:55:02 +0200

    refactor(home/components): remove logic

--- src/app/components/common/wm-common.module.ts
+++ src/app/components/common/wm-common.module.ts
@@ -1,7 +1,4 @@
-import {ConvertToHorizontalScrollBoxItemsPipe} from './horizontal-scroll-box/convert-to-horizontal-scroll-box.pipe';
-import {BoxComponent} from './box/box.component';
 import {CommonModule} from '@angular/common';
-import {ConvertToItemTracksPipe} from './tracks-box/convert-to-base-box.pipe';
 import {ImageModalComponent} from './image-modal/image-modal.component';
 import {IonicModule} from '@ionic/angular';
 import {LayerBoxComponent} from './layer-box/layer-box.component';
@@ -10,24 +7,15 @@ import {PipeModule} from 'src/app/pipes/pipe.module';
 import {PoiBoxComponent} from './poi-box/poi-box.component';
 import {RelatedUrlsComponent} from './related-urls.component';
 import {SearchBoxComponent} from './search-box/search-box.component';
-import {TitleComponent} from './title/title.component';
 import {TrackAudioComponent} from './track-audio/track-audio.component';
-import {TracksBoxComponent} from './tracks-box/tracks-box.component';
-import {HorizontalScrollBoxComponent} from './horizontal-scroll-box/horizontal-scroll-box.component';
 
 const components = [
-  TitleComponent,
   RelatedUrlsComponent,
   ImageModalComponent,
   SearchBoxComponent,
   LayerBoxComponent,
-  TracksBoxComponent,
   PoiBoxComponent,
-  BoxComponent,
-  ConvertToItemTracksPipe,
   TrackAudioComponent,
-  HorizontalScrollBoxComponent,
-  ConvertToHorizontalScrollBoxItemsPipe,
 ];
 @NgModule({
   declarations: components,

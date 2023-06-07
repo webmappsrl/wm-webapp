9af5449f0554f869ca50d104b22e7da927ab5dba
bongiu <peppedeka@gmail.com>
2023-06-07 12:51:23 +0200

    refactor(home): remove logic

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,7 +9,7 @@ import {DomSanitizer} from '@angular/platform-browser';
 import {ActivatedRoute, Router} from '@angular/router';
 import {ModalController, NavController} from '@ionic/angular';
 import {Store} from '@ngrx/store';
-import {BehaviorSubject, Observable} from 'rxjs';
+import {Observable} from 'rxjs';
 import {debounceTime, filter, map, withLatestFrom} from 'rxjs/operators';
 import {
   inputTyped,
@@ -24,14 +24,12 @@ import {
   apiElasticStateLoading,
   apiTrackFilters,
   featureCollection,
-  featureCollectionCount,
   poiFilters,
   queryApi,
   showResult,
 } from 'src/app/shared/wm-core/store/api/api.selector';
 import {confAPP, confHOME} from 'src/app/shared/wm-core/store/conf/conf.selector';
 import {setCurrentPoi} from 'src/app/store/UI/UI.actions';
-import {FiltersComponent} from '../../shared/wm-core/filters/filters.component';
 import {InnerHtmlComponent} from '../project/project.page.component';
 import {SearchComponent} from './search/search.component';
 
@@ -43,25 +41,20 @@ import {SearchComponent} from './search/search.component';
   encapsulation: ViewEncapsulation.None,
 })
 export class HomeComponent implements AfterContentInit {
-  @ViewChild('filterCmp') filterCmp: FiltersComponent;
   @ViewChild('searchCmp') searchCmp: SearchComponent;
 
   confAPP$: Observable<IAPP> = this._store.select(confAPP);
   confHOME$: Observable<IHOME[]> = this._store.select(confHOME);
   currentLayer$ = this._store.select(apiElasticStateLayer);
-  currentSearch$: BehaviorSubject<string> = new BehaviorSubject<string>('');
-  elasticSeachLoading$: Observable<boolean> = this._store.select(apiElasticStateLoading);
-  elasticSearchTracks$: Observable<IHIT[]> = this._store.select(queryApi);
-  isTyping$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-  poiCards$: Observable<any[]> = this._store.select(featureCollection).pipe(
+  poiFilters$: Observable<any> = this._store.select(poiFilters);
+  pois$: Observable<any[]> = this._store.select(featureCollection).pipe(
     filter(p => p != null),
     map(p => ((p as any).features || []).map(p => (p as any).properties || [])),
   );
-  poiFeatureCollectionCount$: Observable<number> = this._store.select(featureCollectionCount);
-  poiFilters$: Observable<any> = this._store.select(poiFilters);
-  selectedTrackFilters$: Observable<any> = this._store.select(apiTrackFilters);
   showResult$ = this._store.select(showResult);
-  showResultType$: BehaviorSubject<string> = new BehaviorSubject<string>('pois');
+  trackFilters$: Observable<any> = this._store.select(apiTrackFilters);
+  trackLoading$: Observable<boolean> = this._store.select(apiElasticStateLoading);
+  tracks$: Observable<IHIT[]> = this._store.select(queryApi);
 
   constructor(
     private _store: Store,
@@ -71,10 +64,6 @@ export class HomeComponent implements AfterContentInit {
     private _navCtrl: NavController,
     public sanitizer: DomSanitizer,
   ) {}
-
-  changeResultType(event): void {
-    this.showResultType$.next(event.target.value);
-  }
 
   goToHome(): void {
     this.setLayer(null);
@@ -154,24 +143,12 @@ export class HomeComponent implements AfterContentInit {
     this._store.dispatch(togglePoiFilter({filterIdentifier}));
   }
 
-  searchCard(id: string | number): void {
-    this._router.navigate([], {
-      relativeTo: this._route,
-      queryParams: {track: id ? id : null},
-      queryParamsHandling: 'merge',
-    });
-  }
-
-  setActivities(filterIdentifier: string): void {
-    this._store.dispatch(toggleTrackFilter({filterIdentifier}));
-  }
-
   setFilter(filterIdentifier: string): void {
     if (filterIdentifier == null) return;
     if (filterIdentifier.indexOf('poi_') >= 0) {
       this._store.dispatch(togglePoiFilter({filterIdentifier}));
     } else {
-      this.setActivities(filterIdentifier);
+      this._store.dispatch(toggleTrackFilter({filterIdentifier}));
     }
   }
 
@@ -199,8 +176,15 @@ export class HomeComponent implements AfterContentInit {
   }
 
   setSearch(value: string): void {
-    this.currentSearch$.next(value);
     this._store.dispatch(inputTyped({inputTyped: value}));
+  }
+
+  setTrack(id: string | number): void {
+    this._router.navigate([], {
+      relativeTo: this._route,
+      queryParams: {track: id ? id : null},
+      queryParamsHandling: 'merge',
+    });
   }
 
   toggleFilter(filterIdentifier: string, idx?: number): void {

18740f2edc3eb910eab4b1035386469ed2c7745a
Rubens Garofalo <rubensgiuseppegarofalo@gmail.com>
2023-06-09 14:34:21 +0200

    ...

--- src/app/components/draw-track/draw-track/draw-track.component.ts
+++ src/app/components/draw-track/draw-track/draw-track.component.ts
@@ -32,7 +32,7 @@ export class DrawTrackComponent {
 
   savedTracks$: BehaviorSubject<any[]> = new BehaviorSubject<any[]>([]);
   track$: BehaviorSubject<any> = new BehaviorSubject<any>(null);
-  selectedTrack: any = null;
+  selectedTrackIdx: number = -1;
 
   constructor(private _http: HttpClient) {
     this._initSavedTracks();
@@ -46,7 +46,6 @@ export class DrawTrackComponent {
       duration: 500,
       maxZoom: this.map.getView().getZoom(),
     });
-    this.selectedTrack = feature;
   }
 
   deleteCustomTrack(id: number): void {

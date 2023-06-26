2e9cd1b38f15d4b0e08cd7c451bd388df605f281
Rubens Garofalo <rubensgiuseppegarofalo@gmail.com>
2023-06-26 16:40:38 +0200

    test:(e2e_home_layerSelected):  implements

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import {defineConfig} from 'cypress';
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:4200',
+    defaultCommandTimeout: 20000,
     testIsolation: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/home/home-layerSelected.spec.cy.ts
+++ cypress/e2e/home/home-layerSelected.spec.cy.ts
@@ -1,11 +1,11 @@
 import {FeatureCollection} from 'geojson';
 import {filterFeatureCollectionByInputTyped as filterFeatureCollectionByInputTypedFn} from '../../../src/app/shared/wm-core/store/api/utils';
-const inputTyped = 'cento';
+
 const confURL = 'https://geohub.webmapp.it/api/app/webmapp/33/config.json';
-const apiURL = 'https://elastic-json.webmapp.it/search/?id=33&query=' + inputTyped;
+const apiURL = 'https://elastic-json.webmapp.it/search/?id=33&layer=164';
 const poisURL = 'https://geohub.webmapp.it/api/v1/app/33/pois.geojson';
 
-describe('HOME_inputTyped', () => {
+describe('HOME_layerSelected', () => {
   let conf = null;
   let elasticRes = null;
   let tracks = [];
@@ -35,19 +35,15 @@ describe('HOME_inputTyped', () => {
       .then(featureCollection => {
         filterFeatureCollectionByInputTyped = filterFeatureCollectionByInputTypedFn(
           featureCollection,
-          inputTyped,
+          '',
         );
         poisCountExpected = filterFeatureCollectionByInputTyped.features.length ?? 0;
       });
 
     cy.visit('/');
-    cy.get('webmapp-search div form input').type(inputTyped);
     cy.intercept('GET', poisURL).as('getPois');
+    cy.get('wm-layer-box').first().click();
     cy.wait('@getPois').its('response.statusCode').should('eq', 200);
-  });
-
-  it('home_inputTyped should exist', () => {
-    cy.get('webmapp-search div form input').should('exist');
   });
 
   it('wm-status-filter: should show "Filtri attivi" button text', () => {
@@ -60,8 +56,19 @@ describe('HOME_inputTyped', () => {
       .last()
       .should('have.text', 'Torna alla home');
   });
+  it('wm-status-filter: should show "Escursionismo" filter', () => {
+    cy.get('wm-status-filter .chip-list ion-chip ion-label')
+      .first()
+      .should('include.text', 'Escursionismo');
+  });
+  it('wm-home-layer: should be 1 layer', () => {
+    cy.get('wm-home-layer').should('have.length', 1);
+  });
   it('wm-home-result: should be 2 tabs', () => {
-    cy.get('wm-home-result > ion-segment > ion-segment-button').should('have.length', 2);
+    cy.get('wm-home-result > ion-segment > ion-segment-button', {timeout: 10000}).should(
+      'have.length',
+      2,
+    );
   });
   //tracks tab
   it(`wm-home-result: first tab should be have same count of tracks`, () => {
@@ -74,25 +81,45 @@ describe('HOME_inputTyped', () => {
     cy.get('wm-home-result ion-content').each((ionContent, idx) => {
       cy.wrap(ionContent)
         .find('wm-search-box') // Seleziona gli elementi <wm-search-box> all'interno di ciascun <wm-horizontal-scroll-box>
-        .should('have.length', tracksCountExpected)
+        .should('have.length', '200' || tracksCountExpected)
+        .then($elements => $elements.slice(0, 10))
         .each((wmSearchBox, idx) => {
-          const currentTrail = tracks[idx]?._source;
-          const name = currentTrail?.name;
-          const distance = currentTrail?.distance;
-          cy.wrap(wmSearchBox)
-            .find('ion-card ion-card-header ion-card-title')
-            .should('include.text', name);
+          const currentTrack = tracks[idx]?._source;
+          const name = currentTrack?.name;
+          const distance = currentTrack?.distance;
+          const taxonomyWhere = currentTrack?.taxonomyWheres;
+          const activity = currentTrack?.activities;
+          if (name) {
+            cy.wrap(wmSearchBox).find('ion-card ion-card-header').should('include.text', name);
+          } else {
+            cy.log(`track ${currentTrack?.id}: missed field name`);
+          }
           if (distance) {
             cy.wrap(wmSearchBox)
               .find('ion-card .wm-search-box-properties .wm-search-box-property')
               .should('include.text', distance);
+          }
+          if (taxonomyWhere) {
+            cy.wrap(wmSearchBox)
+              .find('ion-card ion-card-header ion-card-subtitle')
+              .invoke('text')
+              .then(text => {
+                const taxonomy = taxonomyWhere.find(element => text.includes(element));
+                expect(taxonomy).to.exist;
+                cy.log(`Taxonomy: ${taxonomy}`);
+              });
+          }
+          if (activity) {
+            cy.wrap(wmSearchBox)
+              .find('ion-card .wm-box-taxonomies .wm-box-taxonomy > div')
+              .should('include.text', 'Escursionismo');
           }
         });
     });
   });
   //pois tab
   it(`wm-home-result: second tab should be have same count of pois`, () => {
-    cy.get('wm-home-result > ion-segment > ion-segment-button')
+    cy.get('wm-home-result > ion-segment > ion-segment-button', {timeout: 10000})
       .last()
       .find('span')
       .should('include.text', poisCountExpected);
@@ -100,33 +127,22 @@ describe('HOME_inputTyped', () => {
   it('wm-home-result: pois elem', () => {
     cy.get('wm-home-result > ion-segment > ion-segment-button').last().click();
 
-    cy.get('wm-poi-box').each((wmPoiBox, idx) => {
-      const currentFeature = filterFeatureCollectionByInputTyped.features[idx];
-      const currentProperties = currentFeature.properties;
-      const rawName = JSON.stringify(currentProperties.name);
-      cy.wrap(wmPoiBox)
-        .invoke('text')
-        .then(wmPoiBoxText => {
-          expect(rawName).to.include(wmPoiBoxText);
+    cy.get('wm-home-result ion-content', {timeout: 10000}).each((ionContent, idx) => {
+      cy.wrap(ionContent)
+        .find('div > wm-poi-box')
+        .then($elements => $elements.slice(0, 10)) // Testa solo i primi 100 elementi
+        .each((wmPoiBox, index) => {
+          const currentFeature = filterFeatureCollectionByInputTyped.features[index];
+          const currentProperties = currentFeature.properties;
+          const rawName = JSON.stringify(currentProperties.name);
+          if (rawName) {
+            cy.wrap(wmPoiBox)
+              .invoke('text')
+              .then(wmPoiBoxText => {
+                expect(rawName).to.include(wmPoiBoxText);
+              });
+          }
         });
     });
   });
 });
-
-// TODO: non riesco ad importarla
-const _filterFeatureCollectionByInputTyped = (
-  featureCollection: FeatureCollection,
-  inputTyped: string,
-): FeatureCollection => {
-  if (inputTyped == null || inputTyped == '' || featureCollection == null) {
-    return featureCollection;
-  }
-  return {
-    type: 'FeatureCollection',
-    features: featureCollection.features.filter(feature => {
-      const p = feature?.properties;
-      const searchable = `${JSON.stringify(p?.name ?? '')}${p?.searchable ?? ''}`;
-      return searchable.toLowerCase().indexOf(inputTyped.toLocaleLowerCase()) >= 0;
-    }),
-  } as FeatureCollection;
-};

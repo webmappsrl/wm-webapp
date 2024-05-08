317c8b8c4940c2c26b39e20dcdf798ac03374d67
peppedeka <peppedeka@gmail.com>
2024-05-08 07:51:01 +0200

    chore: Update surge-carg command and API URLs
    - Updated the surge-carg command in package.json to deploy the app to a different domain.
    - Updated the wmMapHitMapUrl in map.page.html to hit a different API endpoint.
    - Updated the API URLs in environment.prod.ts and environment.ts to use a new domain.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   geohubId: 55,
-  api: 'https://carg.dev.maphub.it',
+  api: 'https://carg.maphub.it',
   // graphhopperHost: 'https://graphhopper.sviluppo.lunet.it/',
   graphhopperHost: 'https://graphhopper.webmapp.it/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   geohubId: 55,
-  api: 'https://carg.dev.maphub.it',
+  api: 'https://carg.maphub.it',
   graphhopperHost: 'https://graphhopper.webmapp.it/',
   //graphhopperHost: 'https://graphhopper.sviluppo.lunet.it/',
   // api: 'http://127.0.0.1:8000',

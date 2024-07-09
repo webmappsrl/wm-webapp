1a8dfb5d2a3473ad51665685b5137607b1fc0ff0
Rubens Garofalo <rubensgiuseppegarofalo@gmail.com>
2024-07-09 12:19:20 +0200

    chore: Update configuration service with new map URL. Add 'maps.acquasorgente.cai.it' to the list of map URLs. oc: 3578

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -12,6 +12,7 @@ export class ConfigService {
     'motomappa.motoabbigliament': 53,
     'maps.parcoforestecasentinesi': 49,
     'maps.parcopan': 63,
+    'maps.acquasorgente.cai.it': 58,
   };
 
   public get configUrl(): string {

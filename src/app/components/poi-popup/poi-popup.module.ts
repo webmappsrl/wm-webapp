c7080ccabe064faef052a818465c06da49a7d7bf
Rubens Garofalo <rubensgiuseppegarofalo@gmail.com>
2024-11-15 13:47:58 +0100

    chore(poi-popup): add excerpt display in popup component oc: 4183
    Added a new excerpt section to the POI popup, which conditionally displays content if available. Updated the module imports to include the necessary core module for this feature.

--- src/app/components/poi-popup/poi-popup.module.ts
+++ src/app/components/poi-popup/poi-popup.module.ts
@@ -4,10 +4,11 @@ import {NgModule} from '@angular/core';
 import {PipeModule} from 'src/app/pipes/pipe.module';
 import {PoiPopupComponent} from './poi-popup.component';
 import {WMCommonModule} from '../common/wm-common.module';
+import {WmCoreModule} from 'wm-core/wm-core.module';
 
 @NgModule({
   declarations: [PoiPopupComponent],
-  imports: [CommonModule, PipeModule, WMCommonModule, IonicModule],
+  imports: [CommonModule, PipeModule, WMCommonModule, IonicModule, WmCoreModule],
   exports: [PoiPopupComponent],
 })
 export class PoiPopupModule {}

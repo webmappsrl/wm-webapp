de24bff58dc76baf358f0d4095cdbd5eeeeee191
bongiu <peppedeka@gmail.com>
2025-01-16 15:23:46 +0100

    fix: Add padding and leftPadding to map core
    This commit adds the functionality to set padding and leftPadding values in the map core store using the `padding` and `leftPadding` actions.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ import wmCorePackage from './shared/wm-core/package.json';
 import mapCorePackage from './shared/map-core/package.json';
 import {IAPP, IWEBAPP} from '@wm-core/types/config';
 import {loadAuths} from '@wm-core/store/auth/auth.actions';
+import {leftPadding, padding} from '@map-core/store/map-core.actions';
 @Component({
   selector: 'webmapp-app-root',
   templateUrl: 'app.component.html',
@@ -26,6 +27,8 @@ export class AppComponent implements OnInit {
     this._store.dispatch(loadConf());
     this._store.dispatch(loadAuths());
     this._store.dispatch(ecTracks({init: true}));
+    this._store.dispatch(padding({padding: [100, 100, 100, 100]}));
+    this._store.dispatch(leftPadding({leftPadding: 400}));
     this.confTHEMEVariables$.pipe(take(2)).subscribe(css => this._setGlobalCSS(css));
     console.table({
       'app': appPackage.version,

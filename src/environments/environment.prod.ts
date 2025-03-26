90657daca86da2ff0dbd61fd209fd3dd8f4ea7d6
bongiu <peppedeka@gmail.com>
2025-03-26 15:32:15 +0100

    chore: Update elasticApi URL in environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -19,7 +19,7 @@ export const environment: Environment = {
     },
     camminiditalia: {
       origin: 'https://camminiditalia.maphub.it',
-      elasticApi: 'https://elastic-json.webmapp.it/v2/search',
+      elasticApi: 'https://camminiditalia.maphub.it/api/v2/elasticsearch',
       graphhopperHost: 'https://graphhopper.webmapp.it/',
       awsApi: 'https://wmfe.s3.eu-central-1.amazonaws.com/camminiditalia',
     },
